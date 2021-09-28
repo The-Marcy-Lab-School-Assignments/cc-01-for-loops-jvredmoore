86f26f50ad31a8c80663ff0a629d6e4fc8113931
Jared <Your Github Email Address>
2021-09-28 15:23:00 +0000

    completed two problems of the code challenge

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //1
 function countToFifty(){
-  
+  for(let i = 0; i <= 50; i++){
+     console.log(i)
+  }
 }
 
 //2
 function countFromOne(){
-  
+  for(let i= 1; i <= number; i++){
+     console.log(i)
+  }
 }
 
 //3
@@ -17,5 +21,3 @@ function threeAndFiveInFifty(){
 function threeAndFive(){
   
 }
-
-
